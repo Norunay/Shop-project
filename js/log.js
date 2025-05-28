48e96b09722d43b3a34529f6111561b1d91e15ac
Norunay <002xhero.gg@gmail.com>
2025-05-28 18:55:26 +0500

    tryed to fix but didnt work

--- js/log.js
+++ js/log.js
@@ -1,8 +1,10 @@
+if(localStorage.getItem('isLoggedIn') !== 'true') localStorage.setItem('isLoggedIn', 'false');
+
 let isLogged = localStorage.getItem('isLoggedIn');
 
 const chekker = document.querySelector('.bi-x-lg');
-if(isLogged)chekker.parentElement.style.display = "none";
+if(isLogged == 'true')chekker.parentElement.style.display = "none";
 chekker.onclick = ()=>{
     chekker.parentElement.style.display = "none";
-    localStorage.setItem('isLoggedIn', true);
+    localStorage.setItem('isLoggedIn', 'true');
 }
